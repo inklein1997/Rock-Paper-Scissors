5e9e633b5003237a452889f51282a8c168f66952
Michael Klein <Michaelklein1997@gmail.com>
2021-12-21 23:33:24 -0600

    added if else if conditions for winning and losing

--- script.js
+++ script.js
@@ -6,8 +6,10 @@
 //give confirm telling you score and asking if you want to play again
 
 //declaring variables
-const options = ["R", "P", "S"]
-
+const options = ["R", "P", "S"];
+let computerScore = [];
+let userScore = [];
+let tieScore = [];
 
 
 onload = game();
@@ -15,10 +17,44 @@ onload = game();
 function game () {
     var invite = confirm("Would you like to play rock paper scissors?");
     if (invite = true) {
-        console.log("I would like to play")
+        console.log("I would like to play");
     } else {
-        confirm("Sorry, you don't get a choise.  You HAVE to play!")
+        confirm("Sorry, you don't get a choice.  You HAVE to play!");
     }
 
-    var choice = prompt('Type "R" for rock, "P" for paper and "s" for scissors')
-}
+    var choice = prompt('Type "R" for rock, "P" for paper and "s" for scissors');
+    let random = options[Math.floor(Math.random()*3)];
+    console.log("the computer chose " + random);
+    console.log("you chose " + choice);
+
+    if (choice == random) {
+        alert("The computer chose " + random + "!  Its a tie!");
+
+        } else if (choice == "R" && random == "P") {
+            alert("The computer chose " + random + "!  The computer won!");
+            computerScore.push(1);
+        } else if (choice == "P" && random == "S") {
+            alert("The computer chose " + random + "!  The computer won!");
+            computerScore.push(1);
+        } else if (choice == "S" && random == "R") {
+            alert("The computer chose " + random + "! The computer won!");
+            computerScore.push(1);
+        } else if (choice == "R" && random == "S") {
+            alert("The computer chose " + random + "! Congrats! You won!");
+            userScore.push(1);    
+        } else if (choice == "P" && random == "R") {
+            alert("The computer chose " + random + "! Congrats! You won!");
+            userScore.push(1);    
+        } else if (choice == "S" && random == "P") {
+            alert("The computer chose " + random + "! Congrats! You won!");
+            userScore.push(1);  
+        }
+    var rematch = confirm("Would you like to play again?");
+    if (rematch == true) {
+        console.log("I would like to play again!")
+    }
+
+        console.log("I won " + userScore.length + " times!");
+        console.log("The computer won " + computerScore.length + " times!");
+        console.log("There have been " + tieScore.length + " ties!");
+    }
