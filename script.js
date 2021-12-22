269901beb49da324b766602e3e36ea2a352ca69c
Michael Klein <Michaelklein1997@gmail.com>
2021-12-21 23:05:59 -0600

    added pseudo code and initial game message

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //PSEUDO CODE
 
+//Give confirm asking if you want to play rock paper scissors
+//Give prompt to enter R P or S
+//give alert telling you you lost or won
+//give confirm telling you score and asking if you want to play again
+
+//declaring variables
+const options = ["R", "P", "S"]
+
+
+
+onload = game();
+
+function game () {
+    var invite = confirm("Would you like to play rock paper scissors?");
+    if (invite = true) {
+        console.log("I would like to play")
+    } else {
+        confirm("Sorry, you don't get a choise.  You HAVE to play!")
+    }
+
+    var choice = prompt('Type "R" for rock, "P" for paper and "s" for scissors')
+}
